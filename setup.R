c50e3ec307d06bf26487557fc687147741444437
William Woldum <wilwoldum@gmail.com>
2025-04-25 20:51:58 +0200

    Added missing deps

--- setup.R
+++ setup.R
@@ -1,5 +1,8 @@
 # Kør kun dette en gang
 install.packages("devtools")
 install.packages("dotenv")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("ggplot2")
 library(devtools)
 install_github("cloudyr/limer")
