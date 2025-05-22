0eee04f9617d84cf77b6d340324a84d32602352f
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2025-05-22 13:21:29 +0200

    Made data load from csv instead of limesurvey

--- src/responses.R
+++ src/responses.R
@@ -3,24 +3,23 @@ library(limer)
 source("src/cleaner.R")
 
 # Limer:https://github.com/cloudyr/limer
+# load_dot_env()
+# options(lime_api = Sys.getenv("API_URL"))
+# options(lime_username = Sys.getenv("USERNAME"))
+# options(lime_password = Sys.getenv("PASSWORD"))
+# get_session_key()
+# 
+# responses <- base64_to_df(call_limer(method = "export_responses", params = list(
+#   iSurveyID = 395995, 
+#   sDocumentType = "csv", 
+#   sLanguageCode = "en", 
+#   sCompletionStatus = "all", 
+#   sHeadingType = "code", 
+#   sResponseType = "short")))
+# 
+# release_session_key()
 
-load_dot_env()
-
-options(lime_api = Sys.getenv("API_URL"))
-options(lime_username = Sys.getenv("USERNAME"))
-options(lime_password = Sys.getenv("PASSWORD"))
-
-get_session_key()
-
-responses <- base64_to_df(call_limer(method = "export_responses", params = list(
-  iSurveyID = 395995, 
-  sDocumentType = "csv", 
-  sLanguageCode = "en", 
-  sCompletionStatus = "all", 
-  sHeadingType = "code", 
-  sResponseType = "short")))
-
-release_session_key()
+responses <- read.csv("responses.csv", header = TRUE)
 
 filtered <- filter_responses(responses)
 
